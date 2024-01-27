d60fea9b39693879e1b0cf14cd09979648d247fc
baudoin <baudoinvicbolingo@gmail.com>
2024-01-28 00:31:10 +0200

    linked login

--- src/components/Signup.jsx/Signup.jsx
+++ src/components/Signup.jsx/Signup.jsx
@@ -2,129 +2,135 @@
  import React from "react";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
+import { Link } from "react-router-dom";
 
   const Signup = () => {
     return (
-        <>
+      <>
         <Navbar />
-      <div
-        className="flex items-center justify-center min-h-screen bg-cover " 
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1432821596592-e2c18b78144f?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bG9naW58ZW58MHx8MHx8fDA%3D)",
-        }}
-      >
-        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full" style={{marginTop: '14rem'}}>
-          <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-          <form>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="firstname"
-              >
-                First Name
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="firstname"
-                type="text"
-                placeholder="First Name"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="lastname"
-              >
-                Last Name
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="lastname"
-                type="text"
-                placeholder="Last Name"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                type="email"
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="gender"
-              >
-                Gender
-              </label>
-              <select
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="gender"
-              >
-                <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="dob"
-              >
-                Date of Birth
-              </label>
-              <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="dob"
-                type="date"
-                required
-              />
-            </div>
-            <div className="flex justify-center">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Register
-              </button>
-            </div>
-          </form>
-          <p className="text-gray-600 text-center mt-4">
-            Already have an account?{" "}
-            <a href="/login" className="text-blue-500">
-              Login here
-            </a>
-          </p>
+        <div
+          className="flex items-center justify-center min-h-screen bg-cover "
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1432821596592-e2c18b78144f?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bG9naW58ZW58MHx8MHx8fDA%3D)",
+          }}
+        >
+          <div
+            className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full"
+            style={{ marginTop: "14rem" }}
+          >
+            <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+            <form>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="firstname"
+                >
+                  First Name
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="firstname"
+                  type="text"
+                  placeholder="First Name"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="lastname"
+                >
+                  Last Name
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="lastname"
+                  type="text"
+                  placeholder="Last Name"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="gender"
+                >
+                  Gender
+                </label>
+                <select
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="gender"
+                >
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="dob"
+                >
+                  Date of Birth
+                </label>
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="dob"
+                  type="date"
+                  required
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+            <p className="text-gray-600 text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/Login">
+                <a href="/login" className="text-blue-500">
+                  Login here
+                </a>
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
-       <Footer />
+        <Footer />
       </>
     );
   };
